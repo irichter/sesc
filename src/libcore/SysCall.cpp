1cd9564d9300e6dab5b4644ecc44977a72fa0057
milosp <milosp>
2004-10-20 16:30:30 +0000

    Expanded the list of undoable SysCall types

--- src/libcore/SysCall.cpp
+++ src/libcore/SysCall.cpp
@@ -1,8 +1,18 @@
 #include "SysCall.h"
 
-SysCallMalloc::SysCallMalloc(ThreadContext *context){
-  mySize=context->getGPR(Arg1stGPR);
-  myAddr=context->getHeapManager()->allocate(mySize);
+// Memory management system calls
+
+// For definition of ENOMEM
+#include <errno.h>
+
+void SysCallMalloc::exec(ThreadContext *context, icode_ptr picode){
+  size_t size=context->getGPR(Arg1stGPR);
+  I((!executed)||(mySize==size));
+  ID(mySize=size);
+  I((!executed)||(myPid==context->getThreadPid()));
+  myPid=context->getThreadPid();
+  if(!executed)
+    myAddr=context->getHeapManager()->allocate(size);
   if(myAddr){
     // Map real address to logical address space
     context->setGPR(RetValGPR,context->real2virt(myAddr));
@@ -11,38 +21,486 @@ SysCallMalloc::SysCallMalloc(ThreadContext *context){
     context->setErrno(ENOMEM);
     context->setGPR(RetValGPR,0);
   }
+  /*printf("%1d: exec(%d) SysCallMalloc %8x at
+   * %8x\n",myPid,executed?1:0,mySize,myAddr);*/
+  executed=true;
 }
-void SysCallMalloc::redo(ThreadContext *context){
-  ID(size_t size=context->getGPR(Arg1stGPR));
-  I(size==mySize);
-  if(myAddr){
-    context->getHeapManager()->allocate(myAddr,mySize);
-    // Map real address to logical address space
-    context->setGPR(RetValGPR,context->real2virt(myAddr));
-  }else{
-    // Return 0 and set errno to be POSIX compliant
-    context->setErrno(ENOMEM);
-    context->setGPR(RetValGPR,0);
+void SysCallMalloc::undo(bool expectRedo){
+  I(executed);
+  if((!expectRedo)&&myAddr){
+    I(ThreadContext::getContext(myPid));
+    size_t size=ThreadContext::getContext(myPid)->getHeapManager()->deallocate(myAddr);
+    I(size==mySize);
   }
+/*  printf("%1d: undo(%d) SysCallMalloc %8x at
+ *  %8x\n",myPid,executed?1:0,mySize,myAddr);*/
 }
-void SysCallMalloc::undo(ThreadContext *context){
-  if(myAddr){
-    context->getHeapManager()->deallocate(myAddr);
-  }
-}
-SysCallFree::SysCallFree(ThreadContext *context){
+
+void SysCallFree::exec(ThreadContext *context, icode_ptr picode){
   Address addr=context->getGPR(Arg1stGPR);
   I(addr);
+  I((!executed)||((Address)(context->virt2real(addr))==myAddr));
   myAddr=context->virt2real(addr);
-  mySize==context->getHeapManager()->deallocate(addr);
+  I((!executed)||(myPid==context->getThreadPid()));
+  myPid=context->getThreadPid();
+  if(!executed)
+    mySize=context->getHeapManager()->deallocate(myAddr);
+  /*printf("%1d: exec(%d) SysCallFree   %8x at
+   * %8x\n",myPid,executed?1:0,mySize,myAddr);*/
+  executed=true;
 }
-void SysCallFree::redo(ThreadContext *context){
-  ID(Address addr=context->getGPR(Arg1stGPR));
-  I((Address)(context->virt2real(addr))==myAddr);
-  size_t size=context->getHeapManager()->deallocate(myAddr);
-  I(size==mySize);
+void SysCallFree::undo(bool expectRedo){
+  I(executed);
+  I(ThreadContext::getContext(myPid));
+  /*printf("%1d: undo(%d) SysCallFree   %8x at
+   * %8x\n",myPid,executed?1:0,mySize,myAddr);*/
+  if(!expectRedo){
+    Address addr=ThreadContext::getContext(myPid)->getHeapManager()->allocate(myAddr,mySize);
+    I(addr==myAddr);
+  }
 }
-void SysCallFree::undo(ThreadContext *context){
-  Address addr=context->getHeapManager()->allocate(myAddr,mySize);
-  I(addr==myAddr);
+void SysCallMmap::exec(ThreadContext *context, icode_ptr picode){
+  // Prefered address for mmap. This is ignored in SESC.
+  long addr=context->getGPR(Arg1stGPR);
+  // Size of block to mmap
+  size_t size=context->getGPR(Arg2ndGPR);
+  // Protection flags for mmap
+  int prot=context->getGPR(Arg3rdGPR);
+  // PROT_READ and PROT_WRITE should be set, and nothing lese
+  I(prot==0x3);
+  // Flags for mmap
+  int flag=context->getGPR(Arg4thGPR);
+  // MAP_ANONYMOUS and MAP_PRIVATE should be set, and nothing else
+  I(flag==0x802);
+  I((!executed)||(mySize==size));
+  ID(mySize=size);
+  I((!executed)||(myPid==context->getThreadPid()));
+  if(!executed){
+    myAddr=context->getHeapManager()->allocate(size);
+    if(myAddr==0)
+      myAddr=-1;
+  }
+  if(myAddr!=-1){
+    // Map real address to logical address space
+    context->setGPR(RetValGPR,context->real2virt(myAddr));
+  }else{
+    // Set errno to be POSIX compliant
+    context->setErrno(ENOMEM);
+    context->setGPR(RetValGPR,myAddr);
+  }
+  executed=true;
+}
+void SysCallMmap::undo(bool expectRedo){
+  I(executed);
+  if((!expectRedo)&&(myAddr!=-1)){
+    I(ThreadContext::getContext(myPid));
+    size_t size=ThreadContext::getContext(myPid)->getHeapManager()->deallocate(myAddr);
+    I(size==mySize);
+  }
+}
+void SysCallMunmap::exec(ThreadContext *context, icode_ptr picode){
+  // Starting address of the block
+  long addr=context->getGPR(Arg1stGPR);
+  I(addr);
+  I((!executed)||((Address)(context->virt2real(addr))==myAddr));
+  myAddr=context->virt2real(addr);
+  // Size of block to munmap
+  size_t wantSize=context->getGPR(Arg2ndGPR);
+  I(wantSize);
+  I((!executed)||(myPid==context->getThreadPid()));
+  myPid=context->getThreadPid();
+  if(!executed)
+    mySize=context->getHeapManager()->deallocate(myAddr);
+  I(mySize==wantSize);
+  executed=true;
+}
+void SysCallMunmap::undo(bool expectRedo){
+  I(executed);
+  I(ThreadContext::getContext(myPid));
+  if(!expectRedo){
+    Address addr=ThreadContext::getContext(myPid)->getHeapManager()->allocate(myAddr,mySize);
+    I(addr==myAddr);
+  }
+}
+
+// File I/O system calls
+
+SysCallFileIO::OpenFileVector SysCallFileIO::openFiles;
+
+void SysCallOpen::exec(ThreadContext *context,icode_ptr picode){
+  // Get the file name from versioned memory
+  char pathname[MAX_FILENAME_LENGTH];
+  rsesc_OS_read_string(context->getPid(),picode->addr,pathname, 
+		       (const void *)context->getGPR(Arg1stGPR),
+		       MAX_FILENAME_LENGTH);
+  int flags=conv_flags_to_native((int)context->getGPR(Arg2ndGPR));
+  mode_t mode=(mode_t)context->getGPR(Arg3rdGPR);
+  int newFd=open(pathname,flags,mode);
+  if(executed){
+    I(newFd==myFd);
+  }else{
+    myFd=newFd;
+  }
+  if(myFd==-1){
+    // Open failed, set errno in simulated thread
+    context->setErrno(errno);
+  }else{
+    // Add file's info to vector of currently open files
+    if(openFiles.size()<=(size_t)myFd)
+      openFiles.resize(myFd+1,0);
+    I(openFiles[myFd]==0);
+    openFiles[myFd]=new OpenFileInfo(pathname,flags,mode,0);
+  }
+  context->setGPR(RetValGPR,myFd);
+  executed=true;
+}
+
+void SysCallOpen::undo(bool expectRedo){
+  I(executed);
+  if(myFd!=-1){
+    // Close file and remove from vector of open files
+    int err=close(myFd);
+    I(!err);
+    delete openFiles[myFd];
+    openFiles[myFd]=0;
+  }
+}
+
+void SysCallClose::exec(ThreadContext *context, icode_ptr picode){
+  int fd=context->getGPR(Arg1stGPR);
+  if(executed){
+    I(fd==myFd);
+  }else{
+    myFd=fd;
+  }
+  if((openFiles.size()<=(size_t)fd)||!openFiles[fd]){
+    // Descriptor is not that of an open file
+    if(executed){
+      I(myInfo==0);
+    }else{
+      myInfo=0;
+    }
+    context->setErrno(EBADF);
+  }else{  
+    int err=close(fd);
+    if(err==0){
+      if(executed){
+	I(strcmp(myInfo->pathname,openFiles[fd]->pathname)==0);
+	I(myInfo->flags==openFiles[fd]->flags);
+	I(myInfo->mode==openFiles[fd]->mode);
+	delete myInfo;
+      }
+      // Store info needed to reopen file in undo
+      myInfo=openFiles[fd];
+      // Remove from vector of open files
+      openFiles[fd]=0;
+    }else{
+      I(err==-1);
+      if(executed){
+	I(myInfo==0);
+      }else{
+	myInfo=0;
+      }
+      context->setErrno(errno);
+    }
+  }
+  context->setGPR(RetValGPR,myInfo?0:-1);
+  executed=true;
+}
+
+void SysCallClose::undo(bool expectRedo){
+  I(executed);
+  if(myInfo){
+    int currFD=-1; /* because desired FD could be 0 */ 
+    int fd; 
+    std::stack<int> dummyFDs;     
+    
+    // Ensure the re-opened file has the same file descriptor as before it 
+    // was closed. POSIX open() will always return the lowest available.  So, 
+    // open dummy files until open() returns the desired fd. 
+    int dummyNum;
+    for(dummyNum=0; currFD!=myFd; dummyNum++){ 
+      char *dummyName=(char *)malloc(9*sizeof(char)); 
+      snprintf(dummyName,9,"dummy%d",dummyNum);  
+      currFD=open(dummyName,O_RDONLY|O_CREAT|O_EXCL,0600);  
+      dummyFDs.push(currFD); 
+      free(dummyName); 
+      I(dummyNum<1000); /* probably shouldn't open more than 1000 files.. */ 
+    } 
+    // Close the dummy file that received the fd needed 
+    close(dummyFDs.top());  
+    dummyFDs.pop();  
+    // Reopen the file and update things accordingly 
+    fd=open(myInfo->pathname,myInfo->flags&~O_TRUNC,myInfo->mode);
+    I(fd==myFd);
+    I((openFiles.size()>(size_t)fd)&&!openFiles[fd]);
+    openFiles[fd]=myInfo;
+    if(expectRedo)
+      myInfo=new OpenFileInfo(*(openFiles[fd]));
+    if(openFiles[fd]->offset>0){
+      off_t undoOffs=lseek(fd,openFiles[fd]->offset,SEEK_SET);
+      I(undoOffs==openFiles[fd]->offset);
+    }
+    // Close remaining dummies
+    while(!dummyFDs.empty()){ 
+      close(dummyFDs.top()); 
+      dummyFDs.pop(); 
+    }
+    // Delete dummy files
+    while(dummyNum>=0){
+      char *dummyName=(char *)malloc(9);
+      snprintf(dummyName,9,"dummy%d",dummyNum);
+      remove(dummyName);
+      free(dummyName);
+      dummyNum--;
+    }
+  }
+}
+
+void SysCallClose::done(){
+  if(myInfo)
+    delete myInfo;
+}
+
+void SysCallRead::exec(ThreadContext *context,icode_ptr picode){
+  int fd=context->getGPR(Arg1stGPR);
+  I((!executed)||(fd==myFd));
+  myFd=fd;
+  I((openFiles.size()>(size_t)myFd)&&openFiles[myFd]);
+  void *buf=(void *)(context->getGPR(Arg2ndGPR));
+  I((!executed)||(myBuf==buf));
+  ID(myBuf=buf);
+  size_t count=context->getGPR(Arg3rdGPR);
+  I((!executed)||(myCount==count));
+  ID(myCount=count);
+  void *tempbuff=alloca(count);
+  I(tempbuff);
+  I((!executed)||(oldOffs==lseek(myFd,0,SEEK_CUR)));
+  ID(oldOffs=lseek(myFd,0,SEEK_CUR));
+  ssize_t nowBytesRead=read(myFd,tempbuff,executed?bytesRead:count);
+  I((!executed)||(nowBytesRead==bytesRead));
+  bytesRead=nowBytesRead;
+  context->setGPR(RetValGPR,bytesRead);
+  if(bytesRead==-1){
+    context->setErrno(errno);
+    I(lseek(myFd,0,SEEK_CUR)==oldOffs);
+  }else{
+    I(lseek(myFd,0,SEEK_CUR)==oldOffs+bytesRead);
+    openFiles[myFd]->offset+=bytesRead;
+    rsesc_OS_write_block(context->getPid(),picode->addr,
+			 buf,tempbuff,(size_t)bytesRead);
+  }
+  executed=true;
+}
+void SysCallRead::undo(bool expectRedo){
+  I(executed);
+  if(bytesRead!=-1){
+    I((openFiles.size()>(size_t)myFd)&&openFiles[myFd]);
+    off_t undoOffs=lseek(myFd,-bytesRead,SEEK_CUR);
+    I(undoOffs==oldOffs);
+    I(lseek(myFd,0,SEEK_CUR)==oldOffs);
+  }
+}
+
+void SysCallWrite::exec(ThreadContext *context,icode_ptr picode){
+  int fd=context->getGPR(Arg1stGPR);
+  I((!executed)||(fd==myFd));
+  myFd=fd;
+  void *buf=(void *)context->getGPR(Arg2ndGPR);
+  I((!executed)||(myBuf==buf));
+  ID(myBuf=buf);
+  size_t count=context->getGPR(Arg3rdGPR);
+  I((!executed)||(myCount==count));
+  ID(myCount=count);
+  off_t currentOffset=lseek(myFd,0,SEEK_CUR);
+  if(currentOffset==-1){
+    if(errno==EBADF){
+      // Invalid file handle, fail with errno of EBADF
+      context->setErrno(EBADF);
+      bytesWritten=-1;
+      context->setGPR(RetValGPR,-1);
+    }else if(errno==ESPIPE){
+      // Non-seekable file, must buffer writes instead of undoing them
+      // We can't yet handle open-ed files that are non-seekable
+      I((openFiles.size()<=(size_t)myFd)||!openFiles[myFd]);
+      if(executed){
+	I(bytesWritten==(ssize_t)count);
+	void *tempbuff=alloca(count);
+	I(tempbuff);
+	// Read data from versioned memory into a temporary buffer
+	rsesc_OS_read_block(context->getPid(),picode->addr,tempbuff,buf,count);
+	I(bufData&&(memcmp(bufData,tempbuff,count)==0));
+      }else{
+	bufData=malloc(count);
+	I(bufData);
+	// Read data from versioned memory into a temporary buffer
+	rsesc_OS_read_block(context->getPid(),picode->addr,bufData,buf,count);
+	bytesWritten=count;
+      }
+    }else{
+      I(0);
+    }
+  }else{
+    // File is seekable so writes can be undone
+    I((openFiles.size()>(size_t)myFd)&&openFiles[myFd]);
+    I(!executed||!bufData);
+    bufData=0;
+    void *tempbuff=alloca(count);
+    I(tempbuff);
+    // Read data from versioned memory into a temporary buffer
+    rsesc_OS_read_block(context->getPid(),picode->addr,tempbuff,buf,count);
+    // Get current position and verify that we are in append mode
+    ID(oldOffs=currentOffset);
+    I(oldOffs==lseek(myFd,0,SEEK_END));
+    I(oldOffs==lseek(myFd,0,SEEK_CUR));
+    // Write to file and free the temporary buffer
+    ssize_t nowBytesWritten=write(myFd,tempbuff,executed?bytesWritten:count);
+    I((!executed)||(nowBytesWritten==bytesWritten));
+    bytesWritten=nowBytesWritten;
+    context->setGPR(RetValGPR,bytesWritten);
+    if(bytesWritten==-1){
+      context->setErrno(errno);
+      I(lseek(myFd,0,SEEK_CUR)==oldOffs);
+    }else{
+      I(lseek(myFd,0,SEEK_CUR)==oldOffs+bytesWritten);
+      openFiles[myFd]->offset+=bytesWritten;
+    }
+  }
+  executed=true;
+}
+void SysCallWrite::undo(bool expectRedo){
+  I(executed);
+  if(bufData){
+    I(bytesWritten!=-1);
+    if(!expectRedo)
+      free(bufData);
+  }else if(bytesWritten!=-1){
+    I((openFiles.size()>(size_t)myFd)&&openFiles[myFd]);
+    off_t currOffs=lseek(myFd,-bytesWritten,SEEK_END);
+    I(currOffs==oldOffs);
+    int err=ftruncate(myFd,currOffs);
+    I(!err);
+    I(oldOffs==lseek(myFd,0,SEEK_END));
+    I(oldOffs==lseek(myFd,0,SEEK_CUR));
+  }
+}
+void SysCallWrite::done(void){
+  if(bufData){
+    I(bytesWritten!=-1);    
+    ssize_t nowBytesWritten=write(myFd,bufData,bytesWritten);
+    I(nowBytesWritten==bytesWritten);
+    free(bufData);
+  }
+}
+
+// Thread management system calls
+
+#include "Epoch.h"
+
+void SysCallSescSpawn::exec(ThreadContext *context,icode_ptr picode){
+  // Get parent thread and spawning epoch
+  tls::Epoch *oldEpoch=context->getEpoch();
+  I(oldEpoch==tls::Epoch::getEpoch(context->getPid()));
+  I(oldEpoch);
+  Pid_t ppid=oldEpoch->getTid();
+  // Arguments of the sesc_spawn call
+  RAddr entry = context->getGPR(Arg1stGPR);
+  RAddr arg   = context->getGPR(Arg2ndGPR);
+  long  flags = context->getGPR(Arg3rdGPR);
+  I(entry);
+  ThreadContext *childContext=0;
+  tls::Thread   *childThread=0;
+  tls::Epoch    *childEpoch=0;
+  if(!executed){
+    // Allocate a new thread for the child
+    childContext=ThreadContext::newActual();
+    // Process ID of the child thread
+    childPid=childContext->getPid();
+  }else{
+    childThread=tls::Thread::getByID(static_cast<tls::ThreadID>(childPid));
+    if(!childThread){
+      childContext=ThreadContext::newActual(childPid);
+    }else{
+      childEpoch=childThread->getInitialEpoch();
+    }
+  }
+  // The return value for the parent is the child's pid
+  context->setGPR(RetValGPR,childPid);
+  if(childContext){
+    // Eerything is shared, stack is not copied
+    childContext->shareAddrSpace(context,PR_SADDR,false);
+    childContext->init();
+    // The first instruction for the child is the entry point passed in
+    childContext->setIP(addr2icode(entry));
+    childContext->setGPR(Arg1stGPR,arg);
+    childContext->setGPR(Arg2ndGPR,Stack_size); /* for sprocsp() */
+    // In position-independent code every function expects to find
+    // its own entry address in register jp (also known as t9 and R25)
+    childContext->reg[25]= entry;
+    // When the child returns from the 'entry' function,
+    // it will go directly to the exit() function
+    childContext->setGPR(RetAddrGPR,Exit_addr);
+    // The return value for the child is 0
+    childContext->setGPR(RetValGPR,0);
+    // Inform SESC of what we have done here
+    osSim->eventSpawn(ppid,childPid,flags);
+  }
+  if(!childThread){
+    // Create child's initial epoch
+    childEpoch=tls::Epoch::initialEpoch(static_cast<tls::ThreadID>(childPid),oldEpoch);
+  }else{
+    oldEpoch->changeEpoch();
+    if(!childEpoch){
+      // Re-create child's initial epoch
+      I(0);
+    }
+    childEpoch->run();
+  }
+  executed=true;
+}
+
+void SysCallSescSpawn::undo(bool expectRedo){
+}
+
+void SysCallSescSpawn::done(void){
+}
+
+void SysCallExit::exec(ThreadContext *context,icode_ptr picode){
+  I((!executed)||(myThread==context->getEpoch()->getTid()));
+  myThread=context->getEpoch()->getTid();
+  context->getEpoch()->exitCalled();
+  executed=true;
+}
+
+void SysCallExit::undo(bool expectRedo){
+  I(executed);
+  tls::Thread *thisThread=tls::Thread::getByID(myThread);
+  I(thisThread);
+  thisThread->undoExitCall();
+}
+
+void SysCallWait::exec(ThreadContext *context,icode_ptr picode){
+  if(!executed){
+    childThread=-1;
+  }else if(childThread!=-1){
+    tls::Thread *child=tls::Thread::getByID(childThread);
+    if(!child)
+      return;
+  }
+  context->getEpoch()->waitCalled();
+  executed=true;
+}
+
+void SysCallWait::undo(bool expectRedo){
+  I(executed);
+  if(childThread==-1)
+    return;
+  tls::Thread *child=tls::Thread::getByID(childThread);
+  if(!child)
+    return;
+  tls::ThreadID parentThread=child->getParentID();
+  I(parentThread!=-1);
+  tls::Thread *parent=tls::Thread::getByID(parentThread);
+  I(parent);
+  parent->undoWaitCall(child);
 }
--- src/libcore/SysCall.h
+++ src/libcore/SysCall.h
@@ -1,29 +1,41 @@
 #if !(defined _SysCall_h_)
 #define _SysCall_h_
 
-// For definition of ENOMEM
-#include <errno.h>
-// For the definition of Address
-#include "Snippets.h"
 // For the definition of ThreadContext
 #include "ThreadContext.h"
 
 class SysCall{
+ protected:
+  ID(char *type);
+  bool executed;
+  SysCall(void) : executed(false){
+    ID(type="SysCall");
+  }
  public:
-  virtual void redo(ThreadContext *context) = 0;
-  virtual void undo(ThreadContext *context) = 0;
+  virtual void exec(ThreadContext *context, icode_ptr picode) = 0;
+  virtual void undo(bool expectRedo){};
+  virtual void done(void){};
 };
+
+// Memory management system calls
+
+// For the definition of Address
+#include "Snippets.h"
+
 class SysCallMalloc : public SysCall{
  private:
   // Real address of the allocated block
   // Contains 0 if the malloc call failed
   Address myAddr;
   // Size of the requested allocation
-  size_t  mySize;
+  ID(size_t  mySize);
+  // Process ID of the thread
+  // We need it to get the correct manager in undo
+  Pid_t myPid;
  public:
-  SysCallMalloc(ThreadContext *context);
-  virtual void redo(ThreadContext *context);
-  virtual void undo(ThreadContext *context);
+  ID(SysCallMalloc(void){type="SysCallMalloc";});
+  virtual void exec(ThreadContext *context, icode_ptr picode);
+  virtual void undo(bool expectRedo);
 };
 class SysCallFree : public SysCall{
  private:
@@ -31,10 +43,197 @@ class SysCallFree : public SysCall{
   Address myAddr;
   // Size of the freed block
   size_t  mySize;
+  // Process ID of the thread
+  // We need it to get the correct manager in undo
+  Pid_t myPid;
  public:
-  SysCallFree(ThreadContext *context);
-  virtual void redo(ThreadContext *context);
-  virtual void undo(ThreadContext *context);
+  ID(SysCallFree(void){type="SysCallFree";});
+  virtual void exec(ThreadContext *context, icode_ptr picode);
+  virtual void undo(bool expectRedo);
+};
+class SysCallMmap : public SysCall{
+ private:
+  // Real address of the mmap-ed block
+  // Contains -1 if the mmap call failed
+  Address myAddr;
+  // Size of the requested allocation
+  ID(size_t  mySize);
+  // Process ID of the thread
+  // We need it to get the correct manager in undo
+  Pid_t myPid;
+ public:
+  ID(SysCallMmap(void){type="SysCallMmap";});
+  virtual void exec(ThreadContext *context, icode_ptr picode);
+  virtual void undo(bool expectRedo);  
+};
+class SysCallMunmap : public SysCall{
+ private:
+  // Real address of the unmapped block
+  Address myAddr;
+  // Size of the unmapped block
+  size_t  mySize;
+  // Process ID of the thread
+  // We need it to get the correct manager in undo
+  Pid_t myPid;
+ public:
+  ID(SysCallMunmap(void){type="SysCallMunmap";});
+  virtual void exec(ThreadContext *context, icode_ptr picode);
+  virtual void undo(bool expectRedo);  
+};
+
+// File I/O system calls
+
+// For the definition of rsesc_OS_read_string
+#include "Events.h"
+// For open, close
+#include <fcntl.h>
+// For read, write
+#include <unistd.h>
+// For dummy files when re-getting original fd
+#include <stack>
+#define MAX_FILENAME_LENGTH 64
+
+int conv_flags_to_native(int flags);
+
+class SysCallFileIO : public SysCall{
+ protected:
+  struct OpenFileInfo{
+    int flags;
+    mode_t mode;
+    char pathname[MAX_FILENAME_LENGTH];
+    off_t offset;
+    OpenFileInfo(char *name, int flags, mode_t mode, off_t offset)
+      : flags(flags), mode(mode), offset(offset){
+      I(strlen(name)<MAX_FILENAME_LENGTH);
+      strcpy(pathname,name);
+    }
+    OpenFileInfo(const OpenFileInfo &other)
+      : flags(other.flags), mode(other.mode), offset(other.offset){
+      I(strlen(other.pathname)<MAX_FILENAME_LENGTH);
+      strcpy(pathname,other.pathname);
+    }
+  };
+  // Mapping of an open file's file descriptor to its undo/redo information. 
+  typedef std::vector<OpenFileInfo *> OpenFileVector;
+  static OpenFileVector openFiles;
+ public:
+  virtual void exec(ThreadContext *context, icode_ptr picode) = 0;
+};
+class SysCallOpen : public SysCallFileIO{
+ private:
+  // File descriptor for the opened file, -1 if open failed
+  int myFd;
+ public: 
+  ID(SysCallOpen(void){type="SysCallOpen";});
+  void exec(ThreadContext *context,icode_ptr picode);
+  void undo(bool expectRedo);
+};
+class SysCallClose : public SysCallFileIO{
+ private:
+  int myFd;
+  // Null if close fails, otherwise points to info needed to reopen the file
+  OpenFileInfo *myInfo;
+ public:
+  ID(SysCallClose(void){type="SysCallClose";});
+  virtual void exec(ThreadContext *context, icode_ptr picode);
+  virtual void undo(bool expectRedo);
+  virtual void done(void);
+};
+class SysCallRead : public SysCallFileIO {
+ private:
+  // Descriptor of the file to be read 
+  int myFd;
+  // Return value of call to read (no of bytes actualy read, -1 if error)
+  ssize_t bytesRead;
+  // Remember other params for debugging
+  ID(void *myBuf);
+  ID(size_t myCount);
+  ID(off_t oldOffs);
+ public:
+  ID(SysCallRead(void){type="SysCallRead";});
+  virtual void exec(ThreadContext *context,icode_ptr picode);
+  virtual void undo(bool expectRedo);
+};
+class SysCallWrite : public SysCallFileIO {
+ private:
+  // Descriptor of the file to be written
+  int myFd;
+  // Return value of call to write (no of bytes actually written, -1 if error)
+  ssize_t bytesWritten;
+  // Buffered data for non-seekable devices
+  void *bufData;
+  // Remember other params for debugging
+  ID(void *myBuf);
+  ID(size_t myCount);
+  ID(off_t oldOffs);
+ public:
+  ID(SysCallWrite(void){type="SysCallWrite";});
+  virtual void exec(ThreadContext *context,icode_ptr picode);
+  virtual void undo(bool expectRedo);
+  virtual void done(void);
+};
+
+// Thread management system calls
+
+class SysCallSescSpawn : public SysCall{
+  // ID of the created child thread
+  Pid_t childPid;
+ public:
+  ID(SysCallSescSpawn(void){type="SysCallSescSpawn";});
+  virtual void exec(ThreadContext *context,icode_ptr picode);
+  virtual void undo(bool expectRedo);
+  virtual void done(void);
+};
+
+class SysCallExit : public SysCall{
+ private:
+  Pid_t myThread;
+ public:
+  ID(SysCallExit(void){type="SysCallExit";});
+  virtual void exec(ThreadContext *context,icode_ptr picode);
+  virtual void undo(bool expectRedo);
+};
+
+class SysCallWait : public SysCall{
+ private:
+  Pid_t childThread;
+ public:
+  ID(SysCallWait(void){type="SysCallWait";});
+  virtual void exec(ThreadContext *context,icode_ptr picode);
+  virtual void undo(bool expectRedo);
+  void setChild(Pid_t child){
+    I((childThread==-1)||(child==childThread));
+    childThread=child;
+  }
+  Pid_t getChild(void){
+    return childThread;
+  }
+};
+
+// Time-related system calls
+
+// For the definition of struct tms used in the times() call
+#include <sys/times.h>
+
+class SysCallTimes : public SysCall{
+  clock_t    retVal;
+  struct tms tmsStruct;
+ public:
+  ID(SysCallTimes(void){type="SysCallTimes";});
+  virtual void exec(ThreadContext *context,icode_ptr picode){
+    if(!executed){
+      retVal=globalClock/1024;
+      tmsStruct.tms_utime=globalClock/1024;
+      tmsStruct.tms_stime=0;
+      tmsStruct.tms_cutime=0;
+      tmsStruct.tms_cstime=0;
+    }
+    Address buf=static_cast<Address>(context->getGPR(Arg1stGPR));
+    if(buf)
+      rsesc_OS_write_block(context->getPid(),picode->addr,(void *)buf,&tmsStruct,sizeof(tmsStruct));
+    context->setGPR(RetValGPR,retVal);
+    executed=true;
+  }
 };
 
 #endif // !(defined _SysCall_h_)
